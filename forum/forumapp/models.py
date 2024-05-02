b8fbf2f0d5e9900476176e4adbdf7d0112dba831
magistrum <andejvilcur2312@gmail.com>
2024-05-02 19:36:13 +0300

    start working with git, models

--- forum/forumapp/models.py
+++ forum/forumapp/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 
 class Question(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, null=False, default='Name')
     info = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Answer(models.Model):
+    question_name = models.CharField(max_length=20, null=False, default="Question name")
     text = models.CharField(max_length=500)
